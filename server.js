e4d7fa00d86b5203014eb811a1f9a03e8e0ce5d2
guhyunwoo <realization0327@gmail.com>
2024-07-18 09:15:39 +0900

    complete ws

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const WebSocket = require("ws");
-const path = require("path"); // 추가
-const { Console } = require("console");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -13,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // 정적 파일 제공 설정
-app.use(express.static(path.join(__dirname, "public"))); // 'public' 폴더에 HTML 파일 저장
+app.use(express.static(path.join(__dirname, "public")));
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -27,21 +26,14 @@ db.connect((err) => {
   console.log("MySQL에 연결되었습니다.");
 });
 
-// 로그인 API
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  const query = "SELECT * FROM Users WHERE Username = ? AND Password = ?";
-  db.query(query, [username, password], (err, results) => {
+// 메시지 조회 API
+app.get("/messages", (req, res) => {
+  const query = "SELECT * FROM chats ORDER BY timestamp DESC"; // timestamp를 기준으로 정렬
+  db.query(query, (err, results) => {
     if (err) {
-      res.status(500).json({ error: "데이터베이스 오류" });
-      return;
+      return res.status(500).json({ error: "데이터베이스 오류" });
     }
-    if (results.length > 0) {
-      res.status(200).json({ message: "로그인 성공" });
-    } else {
-      res.status(401).json({ message: "잘못된 사용자명 또는 비밀번호" });
-    }
+    res.json(results); // 저장된 메시지를 JSON 형식으로 응답
   });
 });
 
@@ -50,23 +42,30 @@ const wss = new WebSocket.Server({ port: 9000 });
 
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
-    console.log(message.toString("utf8"));
+    const processeedMessage = message
+      .toString("utf8")
+      .split(":")[2]
+      .split(",")[0]
+      .replace('"', "")
+      .replace('"', "");
 
+    // 데이터베이스 쿼리
     db.query(
-      "insert into Messages (Chatroom_Id, User_Id,  Message_Text) values ('',''))",
-      [username, password],
+      "INSERT INTO chats (chatroom_id, message_text, user_id, nickname) VALUES (?, ?, ?, ?)",
+      [1, processeedMessage, 1, "레전드방송"],
       (err, results) => {
         if (err) {
-          res.status(500).json({ error: "데이터베이스 오류" });
-          return;
+          console.error(err);
+          return; // 오류 발생 시 종료
         }
-        if (results.length > 0) {
-          res.status(200).json({ message: "로그인 성공" });
-        } else {
-          res.status(401).json({ message: "잘못된 사용자명 또는 비밀번호" });
-        }
-      },
-      wss.clients.forEach((client) => {})
+
+        // 메시지 전송 후 클라이언트에게 알림 (옵션)
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(message.toString("utf8"));
+          }
+        });
+      }
     );
   });
 
